a5d445ecb7b3e3b6fd3127da738dafe6564bac25
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 03:58:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'PP'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  pp directors_database
+  pp director_data
   director_data
 end
 
